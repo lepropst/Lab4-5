60f940d0a82bb9f6c97e86e93a330f965c01ddce
lepropst <propstle@gmail.com>
2018-04-30 18:17:16 -0500

    Works on more then one client, starting the DM function.

--- Lab5/src/ChatServer.java
+++ Lab5/src/ChatServer.java
@@ -1,15 +1,14 @@
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
+import java.util.Vector;
 
 public class ChatServer {
-	public final static int PORT = 5562;
-	static RandomAccessFile history;
+	public final static int PORT = 5517;
 	public static void main (String[] args) throws IOException {
-		ArrayList<Object> clients = new ArrayList<Object>();
+		Vector<ChatServerThread> b = new Vector<ChatServerThread>();
 		boolean listen = true;
 		ServerSocket serverSocket = null;
-		String history = null;
+		String username;
 		try {
 			serverSocket = new ServerSocket(PORT);
 		} catch (IOException e) {
@@ -17,22 +16,32 @@ public class ChatServer {
 			System.exit(1);
 		}
 		while (listen) {
-			Socket clientSocket = null;
 			try {
-				clientSocket = serverSocket.accept();
-				System.out.println("Accepted connection");
+				Socket e = serverSocket.accept();
+				DataInputStream dis = new DataInputStream(e.getInputStream());
+				DataOutputStream dos = new DataOutputStream(e.getOutputStream());
+				PrintWriter os = new PrintWriter(new BufferedOutputStream(e.getOutputStream()), true);
+				username = dis.readUTF();
+				System.out.println(username);
+				ChatServerThread g = new ChatServerThread(e, b, username, dos, dis, os);
+				b.add(g);
+				g.start();
+				System.out.println("" + dis);
+				System.out.println("Accepted connection " + e);
+				System.out.println("Vector:" + b);
+
 			} catch (IOException e) {
 				System.out.println("Failed to connect to port " + PORT);
 				continue;
 			}
-			
-			ChatServerThread e;
-			e = new ChatServerThread(clientSocket, history, clients);
-			e.start();
-			clients.add(e);
+
+
+
+
 			System.out.println("Thread Started");
-			System.out.println(e);
+
 		}
+		System.out.println(b);
 		try {
 			serverSocket.accept();
 			System.out.println("Socket closed");
@@ -41,46 +50,61 @@ public class ChatServer {
 		}
 	}
 }
-	class ChatServerThread extends Thread {
-		String inputLine, outputLine;
-		Socket socket = null;
-		ArrayList clients;
-		private String history;
-		
-		
-		ChatServerThread(Socket socket, String history, ArrayList clients) {
-			this.socket = socket;
-			this.history = history;
-			this.clients = clients;
-		}
+class ChatServerThread extends Thread {
+	String inputLine, outputLine, username;
+	Socket socket = null;
+	Vector<ChatServerThread> clients;
+	DataOutputStream dos;
+	DataInputStream dis;
+	PrintWriter pw;
+	/*
+	 * Do I need data input and output streams if I am trying to construct a printwriter
+	 * that the update method uses to print the String history of the chat to the client UI.
+	 */
+	ChatServerThread(Socket socket, Vector<ChatServerThread> clients, String username, DataOutputStream dos, DataInputStream dis, PrintWriter os) {
+		this.socket = socket;
+		this.clients = clients;
+		this.dos = dos;
+		this.dis = dis;
+		this.username = username;
+		this.pw = os;
+	}
+	public void run() {
+		//write();
+		System.out.println("Run is running.");
+		try { 
+			BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-		public String write() {
-			outputLine = inputLine;
-			outputLine = inputLine;
-			return history = history + "\n" + outputLine;
-		}
-	
-		public void run() {
-			
-			try { 
-				BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				PrintWriter os = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
-				
-				while((inputLine = is.readLine()) != null) {
-					
-					for (int i=0; i<clients.size()-1; i++) {
-						clients.get(i);
-						os.println(history);
-					os.flush();
-					
+
+			while((inputLine = is.readLine()) != null) {
+				/*
+				 * 
+				 */
+				//pw.println(inputLine);
+
+				if(inputLine.contains("@")) {
+					String to = inputLine.substring(inputLine.indexOf("@"), inputLine.indexOf(" "));
+					for(int j=0; j<clients.size(); j++) {
+						if(clients.get(j).getName().equals(to)) {
+							clients.get(j).pw.println(inputLine.substring(inputLine.indexOf(" ")));
+						}
+					}
+				} else {
+					for(int i=0; i<clients.size(); i++) {
+
+						clients.get(i).pw.println(inputLine);
+					}
 				}
+				//socket.close();
+			} }catch(IOException e) {
+				System.out.println("I/O Error " + e);
 			}
-				os.close();
-				is.close();
-				socket.close();
-			} catch(IOException e) {
-				System.out.println("I/O Error" + e);
-			}
-		}
-		}
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 	
+}
