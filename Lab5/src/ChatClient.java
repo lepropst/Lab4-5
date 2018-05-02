cb9ae3184d8c6c4de08225ab985e970300677fc5
BGraceCheyney <b.grace.cheyney@tcu.edu>
2018-05-01 20:54:04 -0500

    Added client logic

--- Lab5/src/ChatClient.java
+++ Lab5/src/ChatClient.java
@@ -1,3 +1,7 @@
+import java.net.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.JApplet;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
@@ -8,12 +12,70 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
-public class ChatClient extends JApplet {
+public class ChatClient implements ActionListener {
+	private Socket socket = null;
+	private DataInputStream  console = null;
+	private DataOutputStream streamOut = null;
+
+	public ChatClient(String serverName, int serverPort){
+		System.out.println("Establishing connection. Please wait ...");
+		try{
+			socket = new Socket(serverName, serverPort);
+			System.out.println("Connected: " + socket);
+			start();
+		} catch(UnknownHostException uhe) {
+			System.out.println("Host unknown: " + uhe.getMessage());
+		} catch(IOException ioe) {
+			System.out.println("Unexpected exception: " + ioe.getMessage());
+		}
+		String line = "";
+		while (!line.equals(".bye")) {
+			try {
+				line = console.readLine();
+				streamOut.writeUTF(line);
+				streamOut.flush();
+			} catch(IOException ioe) {
+				System.out.println("Sending error: " + ioe.getMessage());
+			}
+	}
+	}
+	public void start() throws IOException {
+	console   = new DataInputStream(System.in);
+	streamOut = new DataOutputStream(socket.getOutputStream());
+	}
+	public void stop() {
+		try {
+			if (console   != null)  console.close();
+			if (streamOut != null)  streamOut.close();
+			if (socket    != null)  socket.close();
+		}
+		catch(IOException ioe) {
+			System.out.println("Error closing ...");
+		}
+	}
+	public static void main(String args[]) {
+		ChatClient client = null;
+		if (args.length != 2)
+			System.out.println("Usage: java ChatClient host port");
+		else
+			client = new ChatClient(args[0], Integer.parseInt(args[1]));
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
+class ChatUI extends JApplet {
 	private JTextField message;
 	private JTextField server;
 	private JTextField port;
 
-	public ChatClient() {
+	public ChatUI() {
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel controls = new JPanel();
